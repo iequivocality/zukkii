b85081aac669236b8ea5139970b6a52a33e655f3
iequivocality <xanthiandere@gmail.com>
2019-10-19 19:39:12 +0800

    started refactoring

---
+++ src/pages/birthday-countdown/BirthdayCountdown.tsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import styles from './BirthdayCountdownPage.module.scss'
+
+export default class BirthdayCountdownPage extends React.Component {
+    render() {
+        return (<Fragment></Fragment>);
+    }
+}
