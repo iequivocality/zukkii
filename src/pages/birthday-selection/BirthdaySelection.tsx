3f31dac8dc23e3882192656077115148ef36d712
iequivocality <xanthiandere@gmail.com>
2019-10-22 23:08:42 +0800

    group selection

--- src/pages/birthday-selection/BirthdaySelection.tsx
+++ src/pages/birthday-selection/BirthdaySelection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './BirthdaySelection.module.scss';
 import AppState from '../../store/state/AppState';
 import { connect } from 'react-redux';
@@ -18,13 +18,32 @@ class BirthdaySelectionPage extends React.Component<BirthdaySelectionPageProps> 
 
     render() {
         return (
-            <div className={styles.birthdaySelection}>
-                {/* {JSON.stringify(this.props.groupChoices)} */}
-                {this.props.groupChoices.map((group : Group) => {
-                    return <Link to={`/${group.id}`}>{group.name}</Link>
-                })}
-            </div>
+            <Fragment>
+                <div className={styles.titleContainer}>
+                    <h4>アイドルグループ選んでください</h4>
+                </div>
+                <div className={styles.birthdaySelection}>
+                    {/* {JSON.stringify(this.props.groupChoices)} */}
+                    {this.props.groupChoices.map((group : Group) => {
+                        return (
+                            <Link style={this.getGroupStyle(group)} className={styles.groupChoice} to={`/${group.id}`} key={group.id}>
+                                <img className={styles.groupBackground} src={`${process.env.PUBLIC_URL}/images/${group.id}/cover.jpg`}></img>
+                                <div className={styles.groupName}>
+                                    {group.name}
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </div>
+            </Fragment>
         );
+    }
+
+    getGroupStyle(group : Group) {
+        let groupStyle : React.CSSProperties = {
+            backgroundColor: group.color,
+        };
+        return groupStyle
     }
 }
 
